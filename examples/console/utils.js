6fe0e2aedb052869955d9beeb869b4aba8a4ef7d
François Georgy <francois.georgy@gmail.com>
2018-10-04 13:06:42 +0200

    v0.4.9

---
+++ examples/console/utils.js
@@ -0,0 +1,17 @@
+import * as Fretboard from "../../src/fretboard";
+import {Tuning} from "../../src/Tuning.js";
+
+console.log(Fretboard.computeTuningIntervals(Tuning.guitar.standard));
+console.log(Fretboard.computeTuningPitchClasses(Tuning.guitar.standard));
+
+console.log(Fretboard.computeTuningIntervals(Tuning.guitar.standard));
+console.log(Fretboard.computeTuningPitchClasses(Tuning.guitar.standard));
+
+console.log(Fretboard.computeTuningIntervals(Tuning.guitar.drop_d));        // !!! '-2M', '5P', '4P', '4P', '3M', '4P'
+console.log(Fretboard.computeTuningPitchClasses(Tuning.guitar.drop_d));
+
+console.log(Fretboard.computeTuningIntervals(Tuning.guitar7.drop_a));
+console.log(Fretboard.computeTuningPitchClasses(Tuning.guitar7.drop_a));
+
+console.log(Fretboard.computeTuningIntervals(Tuning.bass5.standard));
+console.log(Fretboard.computeTuningPitchClasses(Tuning.bass5.standard));
