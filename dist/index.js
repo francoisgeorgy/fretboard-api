3627da5dae53547d18d5bed5d858a61718b3c154
François Georgy <francois.georgy@gmail.com>
2019-05-25 17:48:05 +0200

    upgrade to babel 7

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.SHAPES = exports.Humanizer = exports.Fretboard = exports.Shape = exports.Tuning = undefined;
+Object.defineProperty(exports, "Tuning", {
+  enumerable: true,
+  get: function get() {
+    return _Tuning.Tuning;
+  }
+});
+exports.Fretboard = exports.Shape = exports.Humanizer = exports.SHAPES = void 0;
 
-var _index = require('./data/index.js');
+var SHAPES = _interopRequireWildcard(require("./data/index.js"));
 
-var SHAPES = _interopRequireWildcard(_index);
+exports.SHAPES = SHAPES;
 
-var _humanizer = require('./humanizer.js');
+var Humanizer = _interopRequireWildcard(require("./humanizer.js"));
 
-var Humanizer = _interopRequireWildcard(_humanizer);
-
-var _shape = require('./shape');
-
-var Shape = _interopRequireWildcard(_shape);
-
-var _fretboard = require('./fretboard');
-
-var Fretboard = _interopRequireWildcard(_fretboard);
-
-var _Tuning = require('./Tuning.js');
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-exports.Tuning = _Tuning.Tuning;
-exports.Shape = Shape;
-exports.Fretboard = Fretboard;
 exports.Humanizer = Humanizer;
-exports.SHAPES = SHAPES; // import * as Utils from './utils.js';
+
+var Shape = _interopRequireWildcard(require("./shape"));
+
+exports.Shape = Shape;
+
+var Fretboard = _interopRequireWildcard(require("./fretboard"));
+
+exports.Fretboard = Fretboard;
+
+var _Tuning = require("./Tuning.js");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
